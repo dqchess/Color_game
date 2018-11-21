9644468de78070405d451da30bafc363667f8a50
Sheerin Naseem <sheerinaseem@Sheerins-MacBook-Pro.local>
2018-11-21 15:07:40 +0530

    Added animation objects and bg sound for begginer level

---
+++ Assets/PlayHints.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayHints : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
